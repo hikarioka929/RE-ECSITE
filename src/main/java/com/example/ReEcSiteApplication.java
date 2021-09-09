d79cf654a2affa115495a77c87133db91f178e48
piyoppa <piyoppa@gmail.com>
2021-09-09 10:48:57 +0900

    first commit

---
+++ src/main/java/com/example/ReEcSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReEcSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReEcSiteApplication.class, args);
+	}
+
+}
