fbdb6860ba3baed14d7691bdc9a631383ab2fa3f
piyoppa <piyoppa@gmail.com>
2021-09-10 14:14:24 +0900

    [add] signOutメソッドの追加 LogoutUserControllerクラスの変更

--- src/main/java/com/example/controller/LogoutUserController.java
+++ src/main/java/com/example/controller/LogoutUserController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,4 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class LogoutUserController {
+	
+	@Autowired
+	private HttpSession session;
+	
+	/**
+	 * ログアウトをする.
+	 * 
+	 * @return 商品一覧画面
+	 */
+	@RequestMapping("/signOut")
+	public String signOut() {
+		session.invalidate();
+		//後で商品一覧画面にリダイレクトするような処理を書く
+		return "item_list_curry";
+	}
 }
