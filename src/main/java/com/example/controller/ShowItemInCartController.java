b95194d5c20ec4414854fae37d971c132026d8eb
piyoppa <piyoppa@gmail.com>
2021-09-17 11:56:31 +0900

    カート内商品表示機能　完成
    [add] ShowItemInCartServiceの新規作成
    
    [add] showItemInCartメソッドの追加
    
    [add] showInCartメソッドの追加
    
    [add] ヘッダーにショッピングカート内を表示するリンクを付与
    
    [modify] showInCartメソッドをinCartメソッドに名前変更
    
    [modify] インデントの調整
    
    [add] ショッピングカート内を表示する機能完成
    
    [modify] addメソッドのリターン先をinCartメソッドへリダイレクトするよう変更
    
    [add] カート内に商品がなかった場合の処理を追加
    
    [fix] ショッピングカート内商品の金額計算が合わない件 getSubTotalメソッドを修正

---
+++ src/main/java/com/example/controller/ShowItemInCartController.java
@@ -0,0 +1,55 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Order;
+import com.example.domain.OrderItem;
+import com.example.domain.User;
+import com.example.service.ShowItemInCartService;
+
+/**
+ * カート内商品表示時に操作するコントローラ.
+ * 
+ * @author okahikari
+ * 
+ */
+@Controller
+@RequestMapping("/item")
+public class ShowItemInCartController {
+
+	@Autowired
+	private ShowItemInCartService showItemInCartService;
+
+	@Autowired
+	private HttpSession session;
+
+	@RequestMapping("/inCart")
+	public String inCart(Model model) {
+		Order order = new Order();
+		User user = (User) session.getAttribute("user");
+		if (user != null) {
+			order = showItemInCartService.showItemInCart(user.getId());
+		} else {
+			order = showItemInCartService.showItemInCart(1);
+		}
+
+		if (order == null) {
+			model.addAttribute("nullMessage", "カートに商品がありません");
+		} else if (order.getOrderItemList().size() == 0) {
+			model.addAttribute("nullMessage", "カートに商品がありません");
+		} else {
+			int totalPrice = 0;
+			for (OrderItem orderItem : order.getOrderItemList()) {
+				totalPrice += orderItem.getSubTotal();
+			}
+			order.setTotalPrice(totalPrice);
+		}
+		model.addAttribute("order", order);
+		return "cart_list";
+	}
+}
