ca8ff6e454c50dc6f054d5ab23116a817bf91e7a
piyoppa <piyoppa@gmail.com>
2021-10-04 12:11:01 +0900

    [add] deleteByOrderItemIdメソッドの追加

--- src/main/java/com/example/repository/OrderToppingRepository.java
+++ src/main/java/com/example/repository/OrderToppingRepository.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
@@ -18,13 +19,13 @@ import com.example.domain.OrderTopping;
  */
 @Repository
 public class OrderToppingRepository {
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate template;
-	
-	private static final RowMapper<OrderTopping> ORDER_TOPPING_ROW_MAPPER
-	= new BeanPropertyRowMapper<>(OrderTopping.class);
-	
+
+	private static final RowMapper<OrderTopping> ORDER_TOPPING_ROW_MAPPER = new BeanPropertyRowMapper<>(
+			OrderTopping.class);
+
 	/**
 	 * 注文トッピングを挿入する.
 	 * 
@@ -34,6 +35,18 @@ public class OrderToppingRepository {
 		StringBuilder sql = new StringBuilder();
 		sql.append("INSERT INTO order_toppings (topping_id, order_item_id) VALUES (:toppingId, :orderItemId);");
 		SqlParameterSource param = new BeanPropertySqlParameterSource(orderTopping);
+		template.update(sql.toString(), param);
+	}
+
+	/**
+	 * 注文トッピングを削除する.
+	 * 
+	 * @param orderItemId 注文商品ID
+	 */
+	public void deleteByOrderItemId(Integer orderItemId) {
+		StringBuilder sql = new StringBuilder();
+		sql.append("DELETE FROM order_toppings WHERE order_item_id = :orderItemId;");
+		SqlParameterSource param = new MapSqlParameterSource().addValue("orderItemId", orderItemId);
 		template.update(sql.toString(), param);
 	}
 }
